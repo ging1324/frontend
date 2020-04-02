17639f880faaaaed1b9df720da5c19420fbb7453
ging1324 <ging1324@gmail.com>
2020-04-02 16:52:02 +0900

    modified detail

--- admin-dashboard/src/components/BoardDetail.vue
+++ admin-dashboard/src/components/BoardDetail.vue
@@ -1,43 +1,59 @@
 <template>
-  <section>
-    <div>
+  <section id="profile">
+    <mdb-row>
       <div>
-        <span>제목 : </span>
-        <h3>{{detailData.title}}</h3>
-      </div>
-      <div>
-        <span>작성자 : </span>
-        <span>{{detailData.author}}</span>
-      </div>
-      <div>
-        <span>내용 : </span>
-        <label for="content"></label>
-        <textarea id="content" v-model="detailData.content"></textarea>
-      </div>
-      <div>
-        <span>채용구분 : </span>
-        <span>{{gubunToText(detailData.gubun)}}</span>
-        <div v-if="detailData.gubun == '0'">
-          <span>{{detailData.start_date}}</span> ~ 
-          <span>{{detailData.end_date}}</span>
+        <div>
+            <h3>{{detailData.title}}</h3>
         </div>
+        <div>
+            <span>작성자 : </span>
+            <p class="detail-content">{{detailData.author}}</p>
+        </div>
+        <div>
+            <span>내용 : </span>
+            <label for="content"></label>
+            <p class="detail-content">{{detailData.content}}</p>
+            <!-- <textarea id="content" v-model="detailData.content"></textarea> -->
+        </div>
+        <div>
+            <span>채용구분 : </span>
+            <p class="detail-content">{{gubunToText(detailData.gubun)}}</p>
+            <div v-if="detailData.gubun == '1'">
+            <span>{{detailData.start_date}}</span> ~ 
+            <span>{{detailData.end_date}}</span>
+            </div>
+        </div>
+        <div>
+            <span>상태</span>
+            <div class="detail-content">{{statusToText(detailData.status)}}</div>
+        </div>
+        <!-- <mdb-btn
+            outline="white"
+            tag="a"
+            href="javascript:;:;"
+            target="_blank"
+            >수정 <mdb-icon icon="graduation-cap" class="ml-2"
+          />
+        </mdb-btn> -->
+        <button type="button" class="btn btn-primary">수정</button>
+          
+        <!-- <div>
+            <span>노출여부</span>
+            <div>{{detailData.is_closed}}</div>
+        </div> -->
       </div>
-      <div>
-        <span>상태</span>
-        <div>{{detailData.status}}</div>
-      </div>
-      <div>
-        <span>노출여부</span>
-        <div>{{detailData.is_closed}}</div>
-      </div>
-    </div>
+    </mdb-row>
   </section>
 </template>
 
 <script>
+import { mdbRow } from 'mdbvue'
+
 export default {
   name: 'BoardDetail',
-  components: {},
+  components: {
+    mdbRow,
+  },
   data() {
     return {
       id:'',
@@ -62,6 +78,13 @@ export default {
       else {
         return '공채';
       }
+    },
+    statusToText(status) {
+        if(status == 0) {
+            return '공개중';
+        }else{
+            return '미공개';
+        }
     }
   },
   mounted() {
@@ -71,5 +94,7 @@ export default {
 </script>
 
 <style>
-
+.detail-content {
+  margin-left: 15px;
+}
 </style>
